630e7e165460c3ec9f31def5a1f4b1204849926a
Jack-0 <github.ay8yf@slmail.me>
2023-06-29 23:07:10 +0100

    configured git signs

--- after/plugin/git-signs.lua
+++ after/plugin/git-signs.lua
@@ -1,10 +1,29 @@
-require('gitsigns').setup{
-  signs = {
+require("gitsigns").setup({
+    --[[ signs = {
     add          = { text = '+' },
-    change       = { text = '│' },
+    change       = { text = '~' },
     delete       = { text = '-' },
     topdelete    = { text = '‾' },
     changedelete = { text = '~' },
     untracked    = { text = 'u' },
-  },
-}
+  }, ]]
+
+    current_line_blame_opts = {
+        delay = 0,
+    },
+
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+        -- binds
+        map("n", "<leader>b", function()
+            gs.toggle_linehl()
+            gs.toggle_numhl()
+            gs.toggle_current_line_blame()
+        end, { desc = "blame" })
+    end,
+})
