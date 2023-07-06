26d9a1d7e0fb4528e68b5060724b95fee867337b
Jack-0 <github.ay8yf@slmail.me>
2023-07-06 20:36:25 +0100

    lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,46 +1,79 @@
-local lsp = require('lsp-zero').preset({
-  manage_nvim_cmp = {
-    set_sources = 'recommended'
-  }
+local lsp = require("lsp-zero").preset({
+	manage_nvim_cmp = {
+		set_sources = "recommended",
+	},
 })
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
+	"tsserver",
+	"eslint",
 	--'sumneko_lua',
-	'rust_analyzer'
+	"rust_analyzer",
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<c-k>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<c-j>'] = cmp.mapping.select_next_item(cmp_select),
-	['<c-i>'] = cmp.mapping.confirm({select = true}), -- was control Y
-	['<c-space>'] = cmp.mapping.complete(),
+	["<c-k>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<c-j>"] = cmp.mapping.select_next_item(cmp_select),
+	["<c-i>"] = cmp.mapping.confirm({ select = true }),
+	["<c-space>"] = cmp.mapping.complete(),
 })
+
+-- sources stop auto completion in ts?
+local sources = {
+	{ name = "nvim_lua" },
+	{ name = "nvim-lsp" },
+	{
+		name = "buffer", --[[, keyword_length = 5 ]]
+	},
+}
+
+local lspkind = require("lspkind")
 
 cmp.setup({
-    mapping = cmp_mappings
+	mapping = cmp_mappings,
+	--sources = sources,
+
+	-- formatting is not working -_-
+	formatting = {
+		format = lspkind.cmp_format({
+			with_text = true,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+			},
+		}),
+	},
 })
 
 --lsp.set_preferences({
 --	sign_icons = { }
 --})
 
-lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+-- TODO: better bindings
+lsp.on_attach(function(_, bufnr)
+	local nmap = function(keys, func, desc)
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, remap = false })
+	end
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    -- lsp buf
+	nmap("<leader>gd", vim.lsp.buf.definition, "goto definition")
+	nmap("<leader>ga", vim.lsp.buf.code_action, "code action")
+	nmap("<leader>gr", vim.lsp.buf.references, "references")
+	nmap("K", vim.lsp.buf.hover, "hover")
+	nmap("<leader>lw", vim.lsp.buf.workspace_symbol, "workspace symbols")
+	nmap("<leader>lh", vim.lsp.buf.signature_help, "signature help")
+
+	-- diagnostics (hints, warnings and errors)
+	nmap("<leader>e", vim.diagnostic.open_float, "open diagnostic")
+	nmap("<leader>le", vim.diagnostic.setloclist, "list diagnostics")
+	nmap("[d", vim.diagnostic.goto_next, "next diagnostic")
+	nmap("]d", vim.diagnostic.goto_prev, "prev diagnostic")
+
 end)
 
 lsp.setup()
