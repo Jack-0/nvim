a0a62e1af01b4d5a3beff5e16189eb63d02fe00e
Jack <github.ay8yf@slmail.me>
2025-01-05 22:26:15 +0000

    hidden files shown grayed out by default

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -36,6 +36,9 @@ return {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
+        filtered_items = {
+          visible=true
+        }
       },
       window = {
         position="current", -- remove this enables the large mode
