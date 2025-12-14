163e7792d5cf7858064e8af2ca7acee92cada7e1
Jack <Jack-0@users.noreply.github.com>
2025-12-14 18:27:29 +0000

    feat: save loc, comment utils

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,4 +3,10 @@ return {
   { "NMAC427/guess-indent.nvim", },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim',  event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  -- uze last place when opening a file
+  { 'farmergreg/vim-lastplace' },
+  -- mixed filetypes etc
+  { 'numToStr/Comment.nvim',                      opts = {} },
+  -- comment strings when multiple languages embedded in file i.e html + js
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
 }
