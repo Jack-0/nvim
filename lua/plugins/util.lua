8b42dea15c2b0b544f10e9d5f6669f89e7481dbc
jack <jack.harris@latticetraining.com>
2025-12-23 17:09:37 +0000

    feat: tag closures

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,12 +1,19 @@
 return {
   -- auto guess tab stuff
-  { "NMAC427/guess-indent.nvim", },
+  { "NMAC427/guess-indent.nvim" },
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim',  event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+  },
   -- uze last place when opening a file
-  { 'farmergreg/vim-lastplace' },
+  { "farmergreg/vim-lastplace" },
   -- mixed filetypes etc
-  { 'numToStr/Comment.nvim',                      opts = {} },
+  { "numToStr/Comment.nvim", opts = {} },
   -- comment strings when multiple languages embedded in file i.e html + js
   { "JoosepAlviste/nvim-ts-context-commentstring" },
+  -- auto tag html
+  { "windwp/nvim-ts-autotag", opts = {} },
 }
