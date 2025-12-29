b9fe100ac7762ea7a902aa87929cff7136c34013
Jack <Jack-0@users.noreply.github.com>
2025-12-29 19:58:52 +0000

    feat: jsx comment support

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -11,9 +11,39 @@ return {
   -- uze last place when opening a file
   { "farmergreg/vim-lastplace" },
   -- mixed filetypes etc
-  { "numToStr/Comment.nvim", opts = {} },
   -- comment strings when multiple languages embedded in file i.e html + js
-  { "JoosepAlviste/nvim-ts-context-commentstring" },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
   -- auto tag html
   { "windwp/nvim-ts-autotag", opts = {} },
+  -- code comments
+  -- this setup allows tsx jsx etc
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      local prehook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+      require("Comment").setup({
+        padding = true,
+        sticky = true,
+        mappings = {
+          basic = true,
+          extra = true,
+          extended = false,
+        },
+        pre_hook = prehook,
+        post_hook = nil,
+      })
+    end,
+    event = "InsertEnter",
+    lazy = true,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+  },
 }
